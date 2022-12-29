f60eb33ba0bc17aabb86f751345c551e6d6be290
shindeabhishek113 <satavsagar17@gmail.com>
2022-12-29 17:09:33 +0530

    demo

---
+++ server.js
@@ -0,0 +1,8 @@
+const express= require("express");
+const app= express();
+const path=require("path");
+ app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"index.html"));
+ });
+  app.listen(1000);
+  console.log("server is chalu")
